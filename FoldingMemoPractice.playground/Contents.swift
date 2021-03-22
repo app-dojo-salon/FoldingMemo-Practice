ed220042a577d58a798e2329125dcab57445c1c0
TakumaOgawa <syuu.2531@gmail.com>
2021-03-22 20:28:16 +0900

    Personクラス作成。

--- FoldingMemoPractice.playground/Contents.swift
+++ FoldingMemoPractice.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var str = "Hello, playground"
+class Person {
+  let name: String
+  let hobby: String
+  let favoriteFoods: [String]
+  
+  init(name: String, hobby: String, favoriteFoods: [String]) {
+    self.name = name
+    self.hobby = hobby
+    self.favoriteFoods = favoriteFoods
+  }
+}
+
+

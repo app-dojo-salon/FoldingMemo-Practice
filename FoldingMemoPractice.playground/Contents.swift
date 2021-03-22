d65c2877f46b3c2439603c6f820b2f110f0d4641
Kei <1372012@g.iuhw.ac.jp>
2021-03-22 22:20:01 +0900

    Update Contents.swift

--- FoldingMemoPractice.playground/Contents.swift
+++ FoldingMemoPractice.playground/Contents.swift
@@ -18,3 +18,18 @@ print(takuma.hobby)
 // ↑自己紹介っぽくしてみました。
 // 何でも良いので追加してPushしてみてください！
 
+class Person2 {
+  let name: String
+  let hobby: String
+  let favoriteFoods: [String]
+  
+  init(name: String, hobby: String, favoriteFoods: [String]) {
+    self.name = name
+    self.hobby = hobby
+    self.favoriteFoods = favoriteFoods
+  }
+}
+
+let kei = Person2(name: "けい", hobby: "Kick-Boxing", favoriteFoods: ["カレー","焼肉"])
+print(kei.hobby)
+
